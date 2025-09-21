18fe4801965ff1640fae785a6774ba29f6407b9c
rashid <rashidrizan8@gamil.com>
2025-09-22 01:32:02 +0530

    added the grpc server in biliing service and grpc client in patient service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,26 +1,33 @@
 package com.pm.billingservice.grpc;
 
+import billing.BillingRequest;
 import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
-    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    private static final Logger log = LoggerFactory.getLogger(
+            BillingGrpcService.class);
 
     @Override
-    public void CreateBillingAccount(billing.BillingRequest billingRequest , StreamObserver<billing.BillingResponse> responseObserver){
-        log.info("Create BillingAccount request received: {}",billingRequest.toString());
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
 
-        //business logic below
+        log.info("createBillingAccount request received {}", billingRequest.toString());
 
-        BillingResponse billingResponse = BillingResponse.newBuilder().setAccountId("12345").setStatus("active").build();
-        responseObserver.onNext(billingResponse);
+        // Business logic - e.g save to database, perform calculates etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
-
-
 }
